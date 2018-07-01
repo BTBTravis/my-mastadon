38c158bc76dd4ad0c23d377d860873e5e8d6bcd8
Travis Shears <btbtravis@gmail.com>
2018-07-01 14:06:41 +0200

    Swap out the api wrapper for simple request

--- handler.js
+++ handler.js
@@ -1,16 +1,26 @@
 'use strict';
+var request = require('request');
+var fastFeed = require('fast-feed');
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.getfeed = (event, context, callback) => {
+  let getFeed = () => new Promise(function(resolve, reject) {
+    request('https://vis.social/@travisshears.atom', function (error, response, body) {
+      if (error || response.statusCode !== 200) reject(error);
+      resolve(body);
+    });
+  });
+  let parseAtom = str => new Promise(function(resolve, reject) {
+    fastFeed.parse(str, function(err, feed) {
+      if (err) throw err;
+      resolve(feed);
+    });
+  });
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  getFeed()
+    .then(str => parseAtom(str))
+    .then(obj => {
+      //callback(null, obj);
+      callback(null, { message: JSON.stringify(obj), event });
+    })
+    .catch(e => console.log('Error!!!'));
 };
